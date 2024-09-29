97b8c616c49a360213ff5b4746054ba049cd898c
donkia <donkia@users.noreply.github.com>
2024-09-29 21:18:43 +0000

    deploy: ee834e00df2e40e9e655f2bf077f17e0f8d7c4e1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -49,7 +49,7 @@ const RESOURCES = {"assets/AssetManifest.json": "bf27638d7c89d797108f66ccdc76676
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"flutter_bootstrap.js": "88952c8f50a339a3cc6005a41352c8cf",
+"flutter_bootstrap.js": "57c30f6a95fb719a0e5578037b9067c8",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "index.html": "cea56c2480827d6279c95e4575ac61da",
