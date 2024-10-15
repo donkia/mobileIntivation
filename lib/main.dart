fb8c6951b26caed1b96702ac8bce44413c3efe55
kimbyeonghyeon <bhkim92@naver.com>
2024-10-15 22:19:56 +0900

    구동 속도 개선

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,9 @@ import 'firebase_options.dart'; // Firebase CLI로 생성된 파일
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
   runApp(const MyApp());
 }
 
@@ -18,11 +18,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const Home(),
+      home: const FirebaseInit(),
       theme: ThemeData(
           fontFamily: 'MaruBuri',
           primaryColor: Colors.white,
           scaffoldBackgroundColor: Colors.white),
+    );
+  }
+}
+
+class FirebaseInit extends StatelessWidget {
+  const FirebaseInit({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Firebase 초기화
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        // 초기화가 완료되었으면 홈 화면으로 이동
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const Home();
+        }
+
+        // 초기화 중일 때 로딩 화면 표시
+        return const Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(), // 로딩 애니메이션
+          ),
+        );
+      },
     );
   }
 }
