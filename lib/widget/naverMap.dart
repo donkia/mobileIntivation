34b64f6666c46610c69dc7e1a775e8eb55431904
kimbyeonghyeon <bhkim92@naver.com>
2024-11-07 22:56:27 +0900

    지도 크기 수정

--- lib/widget/naverMap.dart
+++ lib/widget/naverMap.dart
@@ -19,7 +19,9 @@ class _NaverMapState extends State<NaverMap> {
     ui.platformViewRegistry.registerViewFactory('map-element', (int viewId) {
       final iframe = html.IFrameElement()
         ..src = 'assets/assets/map.html'
-        ..style.border = 'none'; // IFrame 테두리 제거
+        ..style.border = 'none' // IFrame 테두리 제거
+        ..style.width = '100%' // 너비 100%
+        ..style.height = '350px'; // 높이를 원하는 픽셀 값으로 설정
       return iframe;
     });
   }
@@ -27,9 +29,13 @@ class _NaverMapState extends State<NaverMap> {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.all(20.0),
+      padding: EdgeInsets.all(25.0),
       child: Scaffold(
-        body: HtmlElementView(viewType: 'map-element'), // 등록된 viewType 사용
+        body: SizedBox(
+          width: double.infinity,
+          height: 350, // `iframe`의 높이에 맞추어 위젯 높이 설정
+          child: HtmlElementView(viewType: 'map-element'), // 등록된 viewType 사용
+        ),
       ),
     );
   }
