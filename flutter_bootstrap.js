63a500a669c73958695ed02110ae70580fac76b1
donkia <donkia@users.noreply.github.com>
2024-11-10 11:08:35 +0000

    deploy: a89a1486c94696058d87c91cf0a6ac85e099c3c2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1932298425"
+    serviceWorkerVersion: "1564130488"
   }
 });
